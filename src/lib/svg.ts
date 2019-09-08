58ef62bfe801ab19619add4f9c20c3bba42c5aea
zhi <linkasde_system@163.com>
2019-09-09 00:35:04 +0800

    :sparkles: update lib about svg, it can compose path

--- src/lib/svg.ts
+++ src/lib/svg.ts
@@ -34,10 +34,65 @@ class Svg {
 class Element {
   path: string;
 
-  constructor() {}
+  constructor(path: string = '') {
+    this.path = path;
+  }
 
-  lineTo(x: number, y: number) {
-    this.path = ''.concat(this.path, `M ${x}, ${y}`);
+  getPath() {
+    return this.path;
+  }
+
+  setPath(path: string) {
+    this.path = path;
+  }
+
+  lineTo(x: number, y: number, absolute: Boolean = false) {
+    const temp = absolute ? `L ${x}, ${y}` : `l ${x}, ${y}`;
+
+    this.path = ''.concat(this.path, ' ', temp);
+  }
+
+  moveTo(x: number, y: number, absolute: Boolean = true) {
+    const temp = absolute ? `M ${x}, ${y}` : `m ${x}, ${y}`;
+
+    this.path = ''.concat(this.path, ' ', temp);
+  }
+
+  vertical(height: number, absolute: Boolean = false) {
+    const temp = absolute ? `V ${height}` : `v ${height}`;
+
+    this.path = ''.concat(this.path, ' ', temp);
+  }
+
+  horizontal(width: number, absolute: Boolean = false) {
+    const temp = absolute ? `V ${width}` : `v ${width}`;
+
+    this.path = ''.concat(this.path, ' ', temp);
+  }
+
+  close() {
+    this.path = ''.concat(this.path, ' ', 'Z');
+  }
+
+  arc(
+    rx: number,
+    ry: number,
+    angle: number,
+    large: number,
+    sweep: number,
+    x: number,
+    y: number,
+    absolute = false,
+  ) {
+    const temp = absolute
+      ? `A ${rx} ${ry} ${angle} ${large} ${sweep} ${x} ${y}`
+      : `a ${rx} ${ry} ${angle} ${large} ${sweep} ${x} ${y}`;
+
+    this.path = ''.concat(this.path, ' ', temp);
+  }
+
+  curve(path: string) {
+    this.path = ''.concat(this.path, ' ', path);
   }
 }
 
