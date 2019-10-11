574fc3f3643f2754b901ed82a19ec653d59e97d9
zhi <linkasde_system@163.com>
2019-10-12 00:30:56 +0800

    :recycle: update connector in runtime

--- src/engine/runtime.ts
+++ src/engine/runtime.ts
@@ -1,20 +1,52 @@
-import { Connector } from '../modules/connector/index';
+import {
+  Connector,
+  OutputConnector,
+  ConnectorUtils,
+} from '../modules/connector';
+import { ConnectSignalInterface } from '../modules/node';
 import EventEmitter from './eventemitter';
 
+import { MouseEvent } from '../constants';
+import { Workspace } from '../modules/workspace';
+
 class Runtime {
-  connector: Connector;
+  output: Connector;
+  input: Connector;
 
   emitter: EventEmitter;
+  workspace: Workspace;
 
-  constructor(emitter: EventEmitter) {
+  constructor(workspace: Workspace) {
+    const { emitter } = workspace;
     this.emitter = emitter;
+
+    this.workspace = workspace;
 
     this.register();
   }
 
   register() {
-    this.emitter.on('mousedown', (data: Object) => {
-      console.log('dala', data);
+    this.emitter.on('node:connect', (data: ConnectSignalInterface) => {
+      if (!Boolean(this.input)) {
+        this.input = data.connector;
+      }
+    });
+
+    this.emitter.on(MouseEvent.MOUSEMOVE, (event: MouseEvent) => {
+      const { x, y } = event;
+
+      if (Boolean(this.input) && !Boolean(this.output)) {
+        this.output = new OutputConnector(x, y);
+        ConnectorUtils.compose(
+          this.input,
+          this.output,
+          this.workspace.auxiliary,
+        );
+      }
+
+      if (this.output) {
+        this.output.refresh(x, y);
+      }
     });
   }
 }
