676b83e65d916c52790fe1afcfc4c0715ba6dcab
zhi <linkasde_system@163.com>
2019-10-14 00:28:31 +0800

    :recycle: update code's character

--- src/modules/field/index.ts
+++ src/modules/field/index.ts
@@ -1,17 +1,16 @@
-import Field from "./field";
-import OmegaInputField from "./input";
+import Field from './field';
+import OmegaInputField from './input';
 
 const elements = {
-  "omega-input-field": OmegaInputField
+  'omega-input-field': OmegaInputField,
 };
 
 (function install() {
-  console.log("dala");
   const entries = Object.entries(elements);
   entries.forEach(entry => {
     const [name, value] = entry;
     if (!customElements) {
-      console.warn("The bowers not support custom-element !!!");
+      console.warn('The bowers not support custom-element !!!');
       return;
     }
     customElements.define(name, value);
