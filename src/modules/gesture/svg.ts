5249eb0549e132917a09ed5ab3b3db697b1efdca
LinkXSystem <linkasde_system@163.com>
2019-12-21 19:26:32 +0800

    :art: define the module of gesture, it is used to listen for some events

---
+++ src/modules/gesture/svg.ts
@@ -0,0 +1,13 @@
+import { Workspace } from '../workspace';
+
+export default class SVG {
+  workspace: Workspace;
+
+  constructor(workspace) {
+    this.workspace = workspace;
+  }
+
+  handleClick() {}
+
+  handleDbClick() {}
+}
