308a338c8714b6b9aa884eafbc2f106390944a80
LinkXSystem <linkasde_system@163.com>
2020-10-31 19:34:02 +0800

    refactor: define the module of node

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,36 +1,19 @@
 import { NodeSignalConstants } from './emitter';
-
-const Mouse = {
-  MOUSEDOWN: 'mousedown',
-  MOUSEUP: 'mouseup',
-  MOUSEENTER: 'mouseenter',
-  MOUSEMOVE: 'mousemove',
-  MOUSEOVER: 'mouseover',
-  MOUSELEAVE: 'mouseleave',
-  CLICK: 'click'
-};
-
-/**
- * @description set the layer level of an element !
- */
-const ZIndex = {
-  DEFAULT: 0,
-  CANVAS: -1000,
-  SVG: -1,
-  ELEMENT: 1000
-};
-
-const Background = {
-  LINEARGRADIENT: 'linear-gradient(45deg, black, transparent)'
-};
-
-const Position = {
-  ABSOLUTE: 'absolute'
-};
+import { ZIndex, Background, Position } from './style';
+import { Mouse } from "./event";
+import { NodeSymbol } from './node';
 
 const RendererType = {
   SVG: 'svg',
   CANVAS: 'canvas'
 };
 
-export { Mouse, ZIndex, Background, Position, RendererType, NodeSignalConstants };
+export {
+  Mouse,
+  ZIndex,
+  Background,
+  Position,
+  RendererType,
+  NodeSignalConstants,
+  NodeSymbol
+};
---
+++ src/constants/style.ts
@@ -0,0 +1,19 @@
+/**
+ * @description set the layer level of an element !
+ */
+export const ZIndex = {
+  DEFAULT: 0,
+  CANVAS: -1000,
+  SVG: -1,
+  ELEMENT: 1000
+};
+
+export const Background = {
+  LINEARGRADIENT: 'linear-gradient(45deg, black, transparent)',
+  TRANSPARENT: 'transparent'
+};
+
+export const Position = {
+  ABSOLUTE: 'absolute',
+  FIXED: 'fixed'
+};
