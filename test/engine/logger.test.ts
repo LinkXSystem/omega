4aed0a1b6e7ff992048cbd3c1e5dbda3e08ec67b
LinkXSystem <linkasde_system@163.com>
2021-01-25 00:15:11 +0800

    refactor: deleted the test module

--- test/engine/logger.test.ts
+++
@@ -1,9 +0,0 @@
-import Logger from '../../src/engine/logger';
-
-describe('SVG', () => {
-  it('Function Test Of getCubicBezier', () => {
-    Logger.enable('Jest');
-
-    Logger.info('function test !!!');
-  });
-});
